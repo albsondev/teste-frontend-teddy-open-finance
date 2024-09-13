72b413ae14640bda9b08ddc1ec807a1b49d0a08b
André Albson <106167607+albsondev@users.noreply.github.com>
2024-09-13 13:07:58 -0300

    creating the Login component
    creating the Login component (user identification)

---
+++ login-angular/src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  onLogin() {
+    console.log('Usuário:', this.username, 'Senha:', this.password);
+  }
+}
